1d859a0a2bbc5918a75bf549d5c5289237c76fc9
Arjen Oosterkamp <mail@arjen.me>
2013-04-09 13:28:56 +0200

    Added Guard and WithModel

--- Gemfile
+++ Gemfile
@@ -2,10 +2,13 @@ source "http://rubygems.org"
 
 gem 'rake'
 
-# Specify your gem's dependencies in validates_belongs_to.gemspec
 gemspec
 
 group :development do
   gem 'rspec'
   gem 'shoulda-matchers'
+  gem 'guard'
+  gem 'guard-rspec'
+  gem 'rb-fsevent'
+  gem 'with_model'
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,11 @@
-# require 'active_record'
+require 'active_record'
 require 'sunspot_association'
 require 'shoulda/matchers'
+require 'with_model'
+
+RSpec.configure do |config|
+  config.extend WithModel
+end
+
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3",
+                                        :database => File.dirname(__FILE__) + "/sunspot_association.sqlite3")
